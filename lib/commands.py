dec599eb9cc82ecafd5f3e585bcbe39df977d1a5
og2701 <67282414+og2701@users.noreply.github.com>
2024-07-15 19:46:42 +0100

    Update commands.py

--- lib/commands.py
+++ lib/commands.py
@@ -5,7 +5,5 @@ from commands.art_functions.colour_palette import colourPalette
 from commands.art_functions.grid import gridify
 from commands.canvas.screenshot import screenshotCanvas
 
-from commands.dev.user_activity import userActivity
-
 from commands.iceberg.add_to_iceberg import add_iceberg_text
 from commands.iceberg.get_iceberg import show_iceberg
