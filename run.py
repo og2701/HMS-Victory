4d7948a404a797305f581fe84448436006ec6539
og2701 <67282414+og2701@users.noreply.github.com>
2024-07-15 19:45:41 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import os
 from main import client
-from config import TOKEN
 
 if __name__ == "__main__":
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    if TOKEN is None:
+        raise ValueError("No DISCORD_TOKEN found in environment variables")
     client.run(TOKEN)
