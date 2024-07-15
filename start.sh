f412f95952910f6c83f453b7cb027b5c64c0f23f
og2701 <67282414+og2701@users.noreply.github.com>
2024-07-15 19:43:15 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
-pip3 install -r /home/ec2-user/discord_bot/requirements.txt
+pip3 install -r /home/ec2-user/HMS-Victory/requirements.txt
 
 while true
 do
     echo "Starting the Discord bot..."
-    python3 /home/ec2-user/discord_bot/run.py
+    python3 /home/ec2-user/HMS-Victory/run.py
     echo "Bot crashed with exit code $?. Restarting in 5 seconds..."
     sleep 5
 done
