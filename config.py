06d418b30ee7b0ca441568a993693ffae857016c
og2701 <67282414+og2701@users.noreply.github.com>
2024-07-18 18:23:36 +0100

    push config

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
-TOKEN = ""
-
 ROLE_BUTTONS = {
-    "1234567891234567891": {
-        "name": "Role name",
-        "description": "Role description"
+    "1132280073379123311": {
+        "name": "Soldier 🪖",
+        "description": "Receive pings for important events and battles.",
     },
-    "1234567891234567891": {
-        "name": "Role name", 
-        "description": "Role description"
+    "1132951426386116629": {
+        "name": "Night Watch 🦇",
+        "description": "Signify that you will maintain defence of the flag while others sleep.",
+    },
+    "1133022537962491964": {
+        "name": "Voting ✅",
+        "description": "Receive pings for new votes in #🗳️ I voting.",
+    },
+    "1132285964094558288": {
+        "name": "Visitor ✈️",
+        "description": "You're a visitor from another community.",
+    },
+    "1156757081924313161": {
+        "name": "Gardener 🌳",
+        "description": "Receive pings when #🌲 I the-tree needs watering.",
     }
 }
+
