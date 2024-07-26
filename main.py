ff786159c9e579f6c9b9010c8893a39b01084f66
og2701 <67282414+og2701@users.noreply.github.com>
2024-07-26 23:40:10 +0100

    split log functions into new file

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import discord
 from discord import app_commands, Intents, Interaction, Client, InteractionType, Member
 from typing import Optional
-from html2image import Html2Image
-from io import BytesIO
-from PIL import Image, ImageChops
-import uuid
-import requests
-import base64
-import html
-import os
-import difflib
-
+from lib.log_functions import *
 
 from lib.commands import (
     updateRoleAssignments,
@@ -26,8 +17,6 @@ from lib.commands import (
 MINISTER_ROLE_ID = 1250190944502943755
 CABINET_ROLE_ID = 959493505930121226
 LOG_CHANNEL_ID = 959723562892144690
-
-hti = Html2Image(output_path='.')
 
 class AClient(Client):
     def __init__(self):
@@ -50,7 +39,7 @@ class AClient(Client):
             self.synced = True
         print(f"Logged in as {self.user}")
         for command in tree.get_commands():
-            print(command.name)
+            print(f"Command loaded: {command.name}")
 
     async def on_interaction(self, interaction: Interaction):
         if (
@@ -92,8 +81,6 @@ class AClient(Client):
                 with open(image_file_path, "rb") as f:
                     await log_channel.send(file=discord.File(f, "deleted_message.png"), embed=embed)
                 os.remove(image_file_path)
-        else:
-            print(f"Error: Channel with ID {LOG_CHANNEL_ID} not found.")
 
     async def on_message_edit(self, before, after):
         if before.author.bot:
@@ -115,8 +102,6 @@ class AClient(Client):
                 with open(image_file_path, "rb") as f:
                     await log_channel.send(file=discord.File(f, "edited_message.png"), embed=embed)
                 os.remove(image_file_path)
-        else:
-            print(f"Error: Channel with ID {LOG_CHANNEL_ID} not found.")
 
 client = AClient()
 tree = app_commands.CommandTree(client)
@@ -169,107 +154,3 @@ async def add_to_iceberg_command(interaction: Interaction, text: str, level: int
 @tree.command(name="show-iceberg", description="Shows the iceberg image")
 async def show_iceberg_command(interaction: Interaction):
     await show_iceberg(interaction)
-
-
-def trim(im):
-    bg = Image.new(im.mode, im.size, (255, 255, 255))
-    diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
-    if bbox:
-        return im.crop(bbox)
-    return im
-
-def read_html_template(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            return file.read()
-    except Exception as e:
-        print(f"Error reading HTML template {file_path}: {e}")
-        return ""
-
-async def create_message_image(message, title):
-    avatar_url = message.author.avatar.url if message.author.avatar else message.author.default_avatar.url
-    response = requests.get(avatar_url)
-    avatar_base64 = base64.b64encode(response.content).decode('utf-8')
-    avatar_data_url = f"data:image/png;base64,{avatar_base64}"
-    
-    escaped_content = html.escape(message.content)
-    message_lines = escaped_content.split('\n')
-    line_height = 20
-    content_height = line_height * (len(message_lines) + 1)
-    estimated_height = max(100, content_height + 100)
-    border_color = message.author.color.to_rgb()
-    display_name = message.author.display_name
-    created_at = message.created_at.strftime('%H:%M')
-    html_content = read_html_template('templates/deleted_message.html').format(
-        title=title,
-        border_color=border_color,
-        avatar_data_url=avatar_data_url,
-        display_name=display_name,
-        created_at=created_at,
-        content=escaped_content
-    )
-    output_path = f"{uuid.uuid4()}.png"
-    hti.screenshot(html_str=html_content, save_as=output_path, size=(800, estimated_height))
-    image = Image.open(output_path)
-    image = trim(image)
-    image.save(output_path)
-    return output_path
-
-def highlight_diff(before, after):
-    sm = difflib.SequenceMatcher(None, before, after)
-    highlighted_before = []
-    highlighted_after = []
-    changes_detected = False
-    for tag, i1, i2, j1, j2 in sm.get_opcodes():
-        if tag == 'replace':
-            highlighted_before.append(f'<span style="background-color: red;">{html.escape(before[i1:i2])}</span>')
-            highlighted_after.append(f'<span style="background-color: green;">{html.escape(after[j1:j2])}</span>')
-            changes_detected = True
-        elif tag == 'delete':
-            highlighted_before.append(f'<span style="background-color: red;">{html.escape(before[i1:i2])}</span>')
-            changes_detected = True
-        elif tag == 'insert':
-            highlighted_after.append(f'<span style="background-color: green;">{html.escape(after[j1:j2])}</span>')
-            changes_detected = True
-        elif tag == 'equal':
-            highlighted_before.append(html.escape(before[i1:i2]))
-            highlighted_after.append(html.escape(after[j1:j2]))
-    return ''.join(highlighted_before), ''.join(highlighted_after), changes_detected
-
-async def create_edited_message_image(before, after):
-    response = requests.get(before.author.avatar.url)
-    avatar_base64 = base64.b64encode(response.content).decode('utf-8')
-    avatar_data_url = f"data:image/png;base64,{avatar_base64}"
-    escaped_before_content = html.escape(before.content)
-    escaped_after_content = html.escape(after.content)
-    highlighted_before_content, highlighted_after_content, changes_detected = highlight_diff(before.content, after.content)
-    if not changes_detected:
-        return None
-    before_lines = highlighted_before_content.split('\n')
-    after_lines = highlighted_after_content.split('\n')
-    line_height = 20
-    before_content_height = line_height * (len(before_lines) + 1)
-    after_content_height = line_height * (len(after_lines) + 1)
-    content_height = before_content_height + after_content_height + 60
-    estimated_height = max(150, content_height + 100)
-    border_color = before.author.color.to_rgb()
-    display_name = before.author.display_name
-    before_created_at = before.created_at.strftime('%H:%M')
-    after_created_at = after.created_at.strftime('%H:%M')
-    html_content = read_html_template('templates/edited_message.html').format(
-        border_color=border_color,
-        avatar_data_url=avatar_data_url,
-        display_name=display_name,
-        before_created_at=before_created_at,
-        before_content=highlighted_before_content,
-        after_created_at=after_created_at,
-        after_content=highlighted_after_content
-    )
-    output_path = f"{uuid.uuid4()}.png"
-    hti.screenshot(html_str=html_content, save_as=output_path, size=(800, estimated_height))
-    image = Image.open(output_path)
-    image = trim(image)
-    image.save(output_path)
-    return output_path
