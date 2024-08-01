cb5ec175995205cd78203050719d8c5b10c56fb3
og2701 <67282414+og2701@users.noreply.github.com>
2024-08-01 21:08:50 +0100

    add total msgs

--- lib/daily_summary.py
+++ lib/daily_summary.py
@@ -14,17 +14,17 @@ def initialize_summary_data():
                 "members_left": 0,
                 "members_banned": 0,
                 "messages": {},
+                "total_messages": 0,
                 "reactions_added": 0,
                 "reactions_removed": 0,
                 "deleted_messages": 0,
                 "boosters_gained": 0,
                 "boosters_lost": 0,
                 "active_members": defaultdict(int),
-                "reacted_messages": {},
                 "reacting_members": defaultdict(int)
             }, file)
 
-def update_summary_data(key, channel_id=None, user_id=None, message_id=None, message_content=None, author_nickname=None, remove=False):
+def update_summary_data(key, channel_id=None, user_id=None, remove=False):
     with open(SUMMARY_DATA_FILE, "r") as file:
         data = json.load(file)
 
@@ -32,16 +32,11 @@ def update_summary_data(key, channel_id=None, user_id=None, message_id=None, mes
         if str(channel_id) not in data["messages"]:
             data["messages"][str(channel_id)] = 0
         data["messages"][str(channel_id)] += 1
+        data["total_messages"] += 1
     elif key == "active_members" and user_id:
         if str(user_id) not in data["active_members"]:
             data["active_members"][str(user_id)] = 0
         data["active_members"][str(user_id)] += 1
-    elif key == "reacted_messages" and message_id:
-        if str(message_id) not in data["reacted_messages"]:
-            data["reacted_messages"][str(message_id)] = {"count": 0, "content": message_content, "author": author_nickname}
-        data["reacted_messages"][str(message_id)]["count"] += 1 if not remove else -1
-        if data["reacted_messages"][str(message_id)]["count"] <= 0:
-            del data["reacted_messages"][str(message_id)]
     elif key == "reacting_members" and user_id:
         if str(user_id) not in data["reacting_members"]:
             data["reacting_members"][str(user_id)] = 0
@@ -61,13 +56,13 @@ def reset_summary_data():
             "members_left": 0,
             "members_banned": 0,
             "messages": {},
+            "total_messages": 0,
             "reactions_added": 0,
             "reactions_removed": 0,
             "deleted_messages": 0,
             "boosters_gained": 0,
             "boosters_lost": 0,
             "active_members": defaultdict(int),
-            "reacted_messages": {},
             "reacting_members": defaultdict(int)
         }, file)
 
@@ -80,7 +75,6 @@ async def post_daily_summary(client, log_channel_id):
         guild = log_channel.guild
         total_members = guild.member_count
         active_members = sorted(data.get("active_members", {}).items(), key=lambda x: x[1], reverse=True)[:5]
-        reacted_messages = sorted(data.get("reacted_messages", {}).items(), key=lambda x: x[1]["count"], reverse=True)[:5]
         reacting_members = sorted(data.get("reacting_members", {}).items(), key=lambda x: x[1], reverse=True)[:5]
 
         embed = discord.Embed(
@@ -91,6 +85,7 @@ async def post_daily_summary(client, log_channel_id):
         embed.add_field(name="Total Members", value=total_members, inline=False)
         embed.add_field(name="Members Joined", value=data["members_joined"], inline=False)
         embed.add_field(name="Members Left", value=f"{data['members_left']} ({data['members_banned']} banned)", inline=False)
+        embed.add_field(name="Total Messages", value=data["total_messages"], inline=False)
         embed.add_field(name="Reactions Added/Removed", value=f"{data['reactions_added']} / {data['reactions_removed']}", inline=False)
         embed.add_field(name="Deleted Messages", value=data["deleted_messages"], inline=False)
         embed.add_field(name="Boosters (New/Lost)", value=f"{data['boosters_gained']} / {data['boosters_lost']}", inline=False)
@@ -104,21 +99,8 @@ async def post_daily_summary(client, log_channel_id):
             top_members_str = "\n".join([f"<@{user_id}>: {count} messages" for user_id, count in active_members])
             embed.add_field(name="Top 5 Active Members", value=top_members_str, inline=False)
         
-        if reacted_messages:
-            top_reacted_messages = []
-            for message_id, msg_info in reacted_messages:
-                if msg_info:
-                    content = msg_info.get("content", "")
-                    author = msg_info.get("author", "Unknown")
-                    count = msg_info.get("count", 0)
-                    if content and author is not None:
-                        top_reacted_messages.append(f"{content[:50]} by {author}: {count} reactions")
-            top_reacted_messages_str = "\n".join(top_reacted_messages)
-            embed.add_field(name="Top 5 Most Reacted Messages", value=top_reacted_messages_str, inline=False)
-        
         if reacting_members:
             top_reacting_members_str = "\n".join([f"<@{user_id}>: {count} reactions" for user_id, count in reacting_members])
             embed.add_field(name="Top 5 Reacting Members", value=top_reacting_members_str, inline=False)
 
         await log_channel.send(embed=embed)
-
