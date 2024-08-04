d94b88b539875c10a51d76c5e8243851b30e0b35
og2701 <67282414+og2701@users.noreply.github.com>
2024-08-04 23:40:59 +0100

    Update utils.py

--- lib/utils.py
+++ lib/utils.py
@@ -1,13 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import discord
 
 async def restrict_channel_for_new_members(message: discord.Message, channel_id: int, days_required: int = 7):
     if message.channel.id == channel_id:
         join_date = message.author.joined_at
-        if join_date is None or (datetime.utcnow() - join_date).days < days_required:
+        if join_date is None or (datetime.now(timezone.utc) - join_date).days < days_required:
             await message.delete()
             await message.channel.send(
-                f"{message.author.mention}, you need to be in the server for at least a week to use this channel. If you believe you should be whitelisted, please <#1143560594138595439>",
+                f"{message.author.mention}, you need to be in the server for at least {days_required} days to use this channel. If you believe you should be whitelisted, please <#1143560594138595439>",
                 delete_after=10,
             )
             return False
