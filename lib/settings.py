3bbab351ed3b3d23c45724e637e69d56d68b00af
og2701 <67282414+og2701@users.noreply.github.com>
2024-10-07 18:44:36 +0100

    add bio scanner

--- lib/settings.py
+++ lib/settings.py
@@ -16,6 +16,7 @@ class CHANNELS:
 	BOT_SPAM = 968502541107228734
 	POLITICS = 1141097424849481799
 	PORT_OF_DOVER = 1131633452022767698
+	POLICE = 1132970233502650388
 	MEMBER_UPDATES = 1279873633602244668 #on secret server
 	DATA_BACKUP = 1281734214756335757 #on secret server
 	IMAGE_CACHE = 1271188365244497971 #on secret server
@@ -29,3 +30,9 @@ POLITICS_WHITELISTED_USER_IDS = load_whitelist()
 command_usage_tracker = defaultdict(lambda: {'count': 0, 'last_used': None})
 
 SUMMARISE_DAILY_LIMIT = 10
+
+SCAM_KEYWORDS = [
+    "cashapp", "cash app", "payment", "cash", "sugar", "money", "bitcoin", 
+    "btc", "eth", "crypto", "venmo", "paypal", "zelle", "gift card", "western union", "investment", 
+    "profit", "quick cash", "easy money", "giveaway", "prize", "dm ", "referral link", "http", "test"
+]
