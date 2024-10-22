3ce0f8be3fdcdfb2078bca47c7473ed01abe287f
og2701 <67282414+og2701@users.noreply.github.com>
2024-10-22 22:35:02 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+VENV_DIR="/home/ubuntu/HMS-Victory/venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating a virtual environment..."
+    python3 -m venv $VENV_DIR
+fi
+
+source $VENV_DIR/bin/activate
+
 echo "Installing dependencies..."
-python3 -m pip install -r /home/ubuntu/HMS-Victory/requirements.txt
+pip install -r /home/ubuntu/HMS-Victory/requirements.txt
 
 while true
 do
     echo "Starting the Discord bot..."
-    python3 /home/ubuntu/HMS-Victory/run.py
+    python /home/ubuntu/HMS-Victory/run.py
     echo "Bot crashed with exit code $?. Restarting in 5 seconds..."
     sleep 5
 done
-
