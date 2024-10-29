9eeda80196885bd242e50ce3235a9bc9b26e8f16
og2701 <67282414+og2701@users.noreply.github.com>
2024-10-29 22:21:49 +0000

    Update translation.py

--- lib/translation.py
+++ lib/translation.py
@@ -4,7 +4,7 @@ import discord
 
 openai.api_key = os.getenv("OPENAI_TOKEN")
 
-target_language_translations = {
+target_language_mappings = {
     "Over the top 'roadman' speak": "Roadman",
     "Pig speak (e.g oink oink)": "Pig"
 }
@@ -31,7 +31,7 @@ async def translate_and_send(reaction, message, target_language, original_author
         icon_url=original_author.avatar.url if original_author.avatar else original_author.default_avatar.url
     )
 
-    target_language = target_language_translations.get(target_language, target_language)
+    target_language = target_language_mappings.get(target_language, target_language)
 
     embed.set_footer(
         text=f"Translated to {target_language} | Requested by {reacting_user.display_name}"
