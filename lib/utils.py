2034150a300ff1862559fe6e3cd7b37209d2a085
og2701 <67282414+og2701@users.noreply.github.com>
2025-03-07 15:12:52 +0000

    Update utils.py

--- lib/utils.py
+++ lib/utils.py
@@ -332,7 +332,12 @@ async def generate_rank_card(interaction: Interaction, member: Member) -> discor
     size = (1600, 1000)
     output_file = f"{uuid.uuid4()}.png"
     try:
-        hti.screenshot(html_str=html_content, save_as=output_file, size=size)
+        hti.screenshot(
+            html_str=html_content,
+            save_as=output_file,
+            size=(1600, 1000),
+            custom_flags=["--force-device-scale-factor=2"]
+        )
     except Exception as e:
         raise Exception(f"Error taking screenshot: {e}")
     try:
