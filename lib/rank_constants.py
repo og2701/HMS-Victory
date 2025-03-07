352fbc3ce882c2ba29e47e30f7cd76e2f8af6afb
og2701 <67282414+og2701@users.noreply.github.com>
2025-03-07 19:45:33 +0000

    add custom rank

--- lib/rank_constants.py
+++ lib/rank_constants.py
@@ -22,6 +22,7 @@ XP_FILE = "chat_leaderboard.json"
 CUSTOM_RANK_BACKGROUNDS = {
     # "USER_ID_STRING": "custom_background_filename.png",
     "404634271861571584": "oggers.png",
-    "347842997641281536": "blank.png"
+    "347842997641281536": "blank.png",
+    "797207976548499518": "johnny.png"
 }
 
