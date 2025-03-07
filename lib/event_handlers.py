96a0dba283070278a4e9fcea6429332937e50ab1
og2701 <67282414+og2701@users.noreply.github.com>
2025-03-07 00:23:47 +0000

    Update event_handlers.py

--- lib/event_handlers.py
+++ lib/event_handlers.py
@@ -8,7 +8,9 @@ import io
 import json
 import asyncio
 from collections import defaultdict
-from apscheduler.triggers.cron import CronTrigger, IntervalTrigger
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
+
 
 from lib.translation import translate_and_send
 from lib.summary import initialize_summary_data, update_summary_data, post_summary
