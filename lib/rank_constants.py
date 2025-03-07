22c7c46702ce6406b8d718ce53068d648e039cfb
og2701 <67282414+og2701@users.noreply.github.com>
2025-03-07 20:09:46 +0000

    add custom rank

--- lib/rank_constants.py
+++ lib/rank_constants.py
@@ -24,6 +24,7 @@ CUSTOM_RANK_BACKGROUNDS = {
     "404634271861571584": "oggers.png",
     "347842997641281536": "blank.png",
     "797207976548499518": "johnny.png",
-    "725155180680577066": "cherry.png"
+    "725155180680577066": "cherry.png",
+    "1204435534416580679": "gunner.png"
 }
 
