c39fcbb4e19de2146f5ec6b608df92a467ffb1c9
og2701 <67282414+og2701@users.noreply.github.com>
2025-06-30 15:53:31 +0100

    Update economy_state.py

--- commands/economy/economy_state.py
+++ commands/economy/economy_state.py
@@ -12,7 +12,7 @@ async def handle_ukpeconomy_command(interaction: discord.Interaction) -> discord
         if image_path and os.path.exists(image_path):
             with open(image_path, "rb") as f:
                 discord_file = discord.File(f, filename="ukpeconomy_stats.png")
-            # os.remove(image_path)
+            os.remove(image_path)
             return discord_file
         else:
             logger.error("Economy stats image path not found or does not exist.")
