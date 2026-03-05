7771d16ef1d5605e442e3bf1abb6eb6b8e3b65c1
og2701 <67282414+og2701@users.noreply.github.com>
2026-03-05 13:33:45 +0000

    UI Polish: Final layout, progress bar, and title fixes

--- lib/features/xp_system.py
+++ lib/features/xp_system.py
@@ -237,7 +237,7 @@ class XPSystem:
           <div class="flex flex-col gap-2">{right_html}</div>
         </div>
         """
-        final_html = template.replace("{{ LEADERBOARD_ROWS }}", two_col).replace("{{ TITLE }}", "HMS Victory UKPence Richlist")
+        final_html = template.replace("{{ LEADERBOARD_ROWS }}", two_col).replace("{{ TITLE }}", "HMS Victory XP Leaderboard")
         image_buffer = await screenshot_html(final_html, size=(1000, 1200))
         return image_buffer.getvalue()
 
